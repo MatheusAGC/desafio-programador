73e6d287ad833d5c54764a0f15428ccb9cf6d15f
MatheusAGC <matheusancaloni@hotmail.com>
2026-08-17 20:34:51 -0300

    Feito dockerfile e configuracao do docker-compose com api, worker e db

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: 'quickfiller',
-    password: 'quickfiller123',
-    host: 'localhost',
-    port: 5432,
-    database: 'quickfiller',
+  user: 'quickfiller',
+  password: 'quickfiller123',
+  host: process.env.DB_HOST || 'localhost',
+  port: 5432,
+  database: 'quickfiller',
 });
 
 export default pool;
